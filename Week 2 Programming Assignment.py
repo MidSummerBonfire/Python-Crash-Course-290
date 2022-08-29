c8cfd2abdf2121e0df31c16a2e5be0f4394b7f27
Bryan Avila <74707470+MidSummerBonfire@users.noreply.github.com>
2022-08-28 23:21:16 -0700

    Week 2 homework
    Create guest list using List functions and various methods to complete tasks

---
+++ Week 2 Programming Assignment.py
@@ -0,0 +1,26 @@
+"""
+Imagine that you're planning a party and plan to invite ten of your classmates.
+To help with the planning,use the list data structure and initialize it with 10 first names
+from your current SEC290 class roster. If the class size is less than 10, then make the list smaller.
+The initial list should be created with the names inside of it.
+
+Next, use the sort method to sort the list.
+Then display each name, one per line, using a for loop.
+
+Someone whispers in your ear that one of the guests can't make it.
+You'll need to delete this guest and add a replacement.
+
+Use a print statement to display, "Oops! It turns out one of your guests can't make it."
+
+Use the input() function to query the user for the index of the guest that can't make it.
+Since input() always returns a string, you'll need to use the int() function to convert the index provided to an integer.
+
+Use the pop() and the guest's index to remove the guest from the list. Since it's best practice to keep the user informed,
+display a confirmation message to the user including the guest's name.
+Note that pop() returns the value of the element deleted.
+
+Since you now have an open spot, use the input() function to query the user for another guest name and add that name
+to the list using the append() method.
+
+Sort the list again and display each name in another for loop."""
+
